d4454ae4d49a39db4841651ffac20b967132a07b
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2018-03-13 20:26:30 +0530

    -Added the BankerData structure to store the data for an instance of our banker. -Added basic debugging code to print details in the BankerData passed as argument. -Tried to use macros for defining matrices(not working).

--- banker.c
+++ banker.c
@@ -2,7 +2,61 @@
  * This is the banker.
 */
 #include<stdio.h>
-int init(int resourceCount, int *resourcesMAX)
+#include"helper.c"
+// This is a data structure to store all data related to an instance of the banker.
+struct BankerData
+{
+    int availableResourcesCount;
+    int processCount;
+    int* availableResourcesArray;
+    int** resourcesDemandMatrix;
+    int** resourcesAllocatedMatrix;
+    int** resourcesRequiredMatrix;
+};
+/*
+    Takes a BankerData structure and corresponding data to initialize it.
+    Return:  1 on succesfull initialisation.
+            -1 if availableResourcesCount is less than 1.
+    TODO: Check if given values are correct.
+*/
+int init(struct BankerData *data, int availableResourcesCount,int processCount, 
+        int* availableResourcesArray, int** resourcesDemandMatrix, int** resourcesAllocatedMatrix, 
+        int** resourcesRequiredMatrix)
+{
+    if(availableResourcesCount<1)return -1;
+    data->processCount = processCount;
+    data->availableResourcesCount = availableResourcesCount;
+    data->availableResourcesArray = availableResourcesArray;
+    data->resourcesDemandMatrix = resourcesDemandMatrix;
+    data->resourcesAllocatedMatrix = resourcesAllocatedMatrix;
+    data->resourcesRequiredMatrix = resourcesRequiredMatrix;
+    return 1;
+}
+/*
+    Used for debugging.
+    Prints the data of the given banker structure.
+*/
+void displayBanker(struct BankerData *data)
+{
+    int i,j,k;
+    printf("\tBanker Data\n");
+    int** matrices[] = {data->resourcesDemandMatrix,data->resourcesAllocatedMatrix,data->resourcesRequiredMatrix};
+    for(k=0;k<arraylength(matrices);++k)
+    {
+        printf("\t  %s Matrix:-\n",
+        (k==0)?"Recources Demand":(k==1)?"Resources Allocated":"Resources Required");
+        for(i=0;i<data->processCount;++i)
+        {
+            printf("\t\t");
+            for(j=0;j<data->availableResourcesCount;++j)
+            {
+                printf("%d ",matrices[k][i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+int init1(int resourceCount, int *resourcesMAX)
 {
     printf("resourceCount = %d\n",resourceCount);
     return 0;
--- run.c
+++ run.c
@@ -5,5 +5,28 @@ int main()
 {
     printf("Welcome\nThis is the banker's algorithm implementation.\n");
     int resourcesMAX[] = {1,2,3,4,5};
-    init(arraylength(resourcesMAX),resourcesMAX);
+    init1(arraylength(resourcesMAX),resourcesMAX);
+    struct BankerData bd;
+    //TODO: fix this
+    //to2DArray((1,2),(3,4));
+    init(&bd,3,2,
+        //AvailableResourceArray
+        (int[]){1, 2, 3},
+        //resourcesDemandMatrix
+        (int*[]){
+            (int[]){1,1,1},
+            (int[]){3,2,4}
+            },
+        //resourcesAllocatedMatrix
+        (int*[]){
+            (int[]){0,0,0},
+            (int[]){0,0,0}
+            },
+        //resourcesRequiredMatrix
+        (int*[]){
+            (int[]){0,0,0},
+            (int[]){0,8,0}
+            }
+        );
+    displayBanker(&bd);
 }
